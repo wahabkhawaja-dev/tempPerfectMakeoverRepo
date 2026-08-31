88245ba691075f6ccc00a5247f02ffb824f2b42c
wahabkhawaja-dev <wahabkhawaja@gamedistrict.co>
2026-08-31 17:07:03 +0500

    Playable with bigger icon uploaded

--- LunaTemp/stage3/processed-scripts/Assets/_Core/Scripts/Generic/Statics.cs
+++
@@ -1,8 +0,0 @@
-public static class Statics
-{
-    public static void GA_LevelStartEvent(string eventName) { }
-
-    public static void GA_LevelCompleteEvent(string eventName) { }
-
-    public static void GA_CustomStringEvent(string eventName) { }
-}
